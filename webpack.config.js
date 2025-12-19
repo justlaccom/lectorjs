25daf3193e762dadb7562053337593e2016dff51
laccom <bzh78480@gmail.com>
2025-12-19 17:43:14 +0100

    Mise à jour de la configuration Webpack pour une meilleure gestion des exports

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: './src/lector.js',
   output: {
     filename: 'lector.min.js',
     path: path.resolve(__dirname, 'dist'),
     library: 'Lector',
     libraryTarget: 'umd',
+    globalObject: 'this',
     umdNamedDefine: true
   },
   module: {
@@ -24,6 +26,16 @@ module.exports = {
   },
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin()],
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
   },
+  externals: {
+    // Empêche le chargement de dépendances inutiles
+    // qui pourraient causer des problèmes de chargement
+    'fs': 'commonjs fs',
+    'path': 'commonjs path'
+  }
 };
